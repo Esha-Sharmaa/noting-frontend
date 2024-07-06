f02a2a7cf8e3571b3d3373c1dcad13af6b537405
Esha Sharma <eshasharmakb22@gmail.com>
2024-07-06 21:13:42 +0530

    change font to popins

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Capriola", "sans-serif"],
+        sans: ["Poppins", "sans-serif"],
         bungee: ['"Bungee Inline"', "cursive"],
       },
       colors: {
